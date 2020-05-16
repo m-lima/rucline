a5269cff56eac21d3d4adf26b3bc3ab01f260e3e
Marcelo Lima <marcelowind@gmail.com>
2020-05-16 15:58:17 +0200

    Test navigation

--- src/prompt/navigation.rs
+++ src/prompt/navigation.rs
@@ -68,43 +68,122 @@ pub(super) fn previous_word_end(pivot: usize, string: &[char]) -> usize {
     }
 }
 
-// // TODO
-// #[cfg(test)]
-// mod test {
-//     use crate::prompt::char_string::CharString;
-//
-//     const EMPTY: CharString = CharString::new();
-//     const WHITE_SPACE: CharString = CharString::from("   \t   ");
-//     const PRE_WHITE_SPACE: CharString = CharString::from("AddZ   \t   ");
-//     const POST_WHITE_SPACE: CharString = CharString::from("   \t   AddZ");
-//     const INTRA_WHITE_SPACE: CharString = CharString::from("   \t   AddZ   \t   ");
-//     const MULTIPLE_WORDS_A: CharString = CharString::from("AddZ AdZ  AZ   A AZ  AdZ   AddZ");
-//     const MULTIPLE_WORDS_Z: CharString = CharString::from("AddZ AdZ  AZ   Z AZ  AdZ   AddZ");
-//
-//     enum Allowance {
-//         Start,
-//         End,
-//     }
-//
-//     fn test_scenario<F: Fn(usize, &[char]) -> usize>(target: F, allowance: Allowance) {
-//         {
-//             let index = 0;
-//             while let index
-//         }
-//     }
-//
-//     // fn test_scenario(allowance: Allowance) {
-//     //     allowed_index = mat
-//     // }
-//
-//     #[test]
-//     fn next_word() {
-//         use super::next_word as uut;
-//     }
-//
-//     #[test]
-//     fn previous_word() {}
-//
-//     #[test]
-//     fn previous_word_end() {}
-// }
+#[cfg(test)]
+mod test {
+    use crate::prompt::char_string::CharString;
+
+    #[derive(Copy, Clone)]
+    enum Direction {
+        Forward,
+        Backward,
+    }
+
+    impl Direction {
+        pub(super) fn start_for(self, scenario: &CharString) -> usize {
+            match self {
+                Direction::Forward => 0,
+                Direction::Backward => scenario.len(),
+            }
+        }
+    }
+
+    struct Tester {
+        direction: Direction,
+        scenarios: [CharString; 6],
+    }
+
+    impl Tester {
+        pub(super) fn prepare(direction: Direction) -> Self {
+            Self {
+                direction,
+                scenarios: [
+                    CharString::new(),
+                    CharString::from("   \t   "),
+                    CharString::from("AddZ   \t   "),
+                    CharString::from("   \t   AddZ"),
+                    CharString::from("   \t   AddZ   \t   "),
+                    CharString::from("AddZ AdZ  AZ   O AZ  AdZ   AddZ"),
+                ],
+            }
+        }
+
+        pub(super) fn test<F, V>(&self, uut: F, validator: V)
+        where
+            F: Fn(usize, &[char]) -> usize,
+            V: Fn(usize, &[char]) -> bool,
+        {
+            for scenario in &self.scenarios {
+                test_scenario(
+                    &uut,
+                    &validator,
+                    self.direction,
+                    &scenario,
+                    self.direction.start_for(&scenario),
+                    0,
+                )
+            }
+        }
+    }
+
+    fn test_scenario<F, V>(
+        uut: F,
+        validator: V,
+        direction: Direction,
+        scenario: &CharString,
+        start: usize,
+        iteration: usize,
+    ) where
+        F: Fn(usize, &[char]) -> usize,
+        V: Fn(usize, &[char]) -> bool,
+    {
+        let pivot = uut(start, scenario);
+
+        match direction {
+            Direction::Forward => {
+                if pivot == scenario.len() {
+                    return;
+                }
+                assert!(pivot > start);
+            }
+            Direction::Backward => {
+                if pivot == 0 {
+                    return;
+                }
+                assert!(pivot < start);
+            }
+        }
+
+        assert!(
+            validator(pivot, scenario),
+            "failed on iteration {} at index {} for {}",
+            iteration,
+            pivot,
+            scenario
+        );
+        test_scenario(uut, validator, direction, scenario, pivot, iteration + 1);
+    }
+
+    #[test]
+    fn next_word() {
+        let tester = Tester::prepare(Direction::Forward);
+        tester.test(super::next_word, |pivot, string| {
+            string[pivot] == 'A' || string[pivot] == 'O'
+        });
+    }
+
+    #[test]
+    fn previous_word() {
+        let tester = Tester::prepare(Direction::Backward);
+        tester.test(super::previous_word, |pivot, string| {
+            string[pivot] == 'A' || string[pivot] == 'O'
+        });
+    }
+
+    #[test]
+    fn previous_word_end() {
+        let tester = Tester::prepare(Direction::Backward);
+        tester.test(super::previous_word_end, |pivot, string| {
+            string[pivot - 1] == 'Z' || string[pivot - 1] == 'O'
+        });
+    }
+}
